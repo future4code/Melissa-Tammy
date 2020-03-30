5437416ffd2414d441b2de5aec5f0006dcaaead9
Melissa Tammy Melonio <mel.tammy2@gmail.com>
2020-03-30 15:38:52 -0300

    Exercício aula 9 ok

---
+++ semana3/logica-booleana/js-vanilla-template/src/index.js
@@ -0,0 +1,111 @@
+// 1.Leia o código abaixo. Indique todas as mensagens impressas no console.
+// const bool1 = true
+// const bool2 = false
+// const bool3 = !bool2
+
+// let resultado = bool1 && bool2 && bool3
+// console.log("a. ", resultado)
+
+// resultado = (bool2 || bool1) && !bool3
+// console.log("b. ", resultado)
+
+// resultado = !resultado && (bool1 || bool1)
+// console.log("c. ", resultado)
+
+// resultado = (resultado && (!bool1 || bool2)) && !bool3
+// console.log("d. ", resultado)
+
+// console.log("e. ", typeof resultado)
+
+// a.  false
+// b.  false
+// c.  true
+// d.  false
+// e.  boolean
+
+// let array
+// console.log('I. ', array)
+
+// array = null
+// console.log('II. ', array)
+
+
+// array = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+// console.log('III. ', array.length)
+
+// let i = 0
+// console.log('IV. ', array[i], " e ", array[i+1])
+
+// array[i+1] = 19
+// const valor = array[i+6]
+// console.log('V. ', array[i+1], " e ", valor)
+
+// i+=1
+// array[i] = array[i-1]
+// console.log('VI. ', array[i])
+
+// i = array.length - 1
+// array[i] = array[i-3]
+// const resultadoC = array[i]%array[1]
+// console.log('VII. ', resultadoC)
+
+// a. O que é `array` e como se declara em `JS`?
+// É uma estrutura de dados que armazena uma coleção de elementos.
+// É declarado com: Array = [<dados>]
+
+// b. Qual o index inicial de um `array`?
+// Undefined
+
+// c. Como se determinar o tamanho do `array`?
+// Com a quantidade de dados inserida na array, pose ser consultada com array.length
+
+// d. Indique todas as mensagens impressas no console.
+// I.  undefined
+// II.  null
+// III.  11
+// IV.  3  e  4
+// V.  19  e  9
+// VI.  3
+// VII.  1
+
+
+// let num = 77;
+// let kelvin = ((num) - 32)*5/9 + 273.15;
+// console.log(kelvin + "K");
+// num = 80;
+// let fahr = (((num)/5)*9)+ 32;
+// console.log(fahr + "°F");
+// num = prompt();
+// fahr = (((num)/5)*9)+ 32;
+// console.log(fahr + "°F")
+// num = fahr;
+// kelvin = ((num) - 32)*5/9 + 273.15;
+// console.log(kelvin + "K")
+
+
+// harry = prompt("As respostas inseridas substituirão uma palavra de um nome de filme. Qual é o objeto mais próximo a você?");
+// summer = prompt("Qual foi a ultima doença que você teve?")
+// brilho = prompt("A ultima coisa que você comeu")
+// futuro = prompt("Um inseto que você não gosta")
+// milagre = prompt("Alguma bebida")
+
+// console.log("Harry Potter e a " + harry + " filosofal")
+// console.log("500 dias com " + summer)
+// console.log("Brilho eterno de uma mente sem " + brilho )
+// console.log("Exterminador do " + futuro)
+// console.log("A espera de uma/um " + milagre )
+
+// let quilo = 280;
+// pagar = quilo * 0.05;
+// console.log("Total a pagar: " + pagar);
+// console.log("Total a pagar com desconto: " + pagar*0.85);
+
+let quilo = 20/2.2046;
+let quilo2 =  10.5/35.274;
+console.log("20lb equivalem a " + quilo + " kg" + "10.5oz equivalem a " + quilo2 + "kg"  ) 
+console.log("20lb equivalem a " + quilo + " kg" + "10.5oz equivalem a " + quilo2 + "kg"  ) 
+
+
+
+
+
