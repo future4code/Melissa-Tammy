f2db0596cbcb82bf351442c8ade5b3f4a7865416
Melissa Tammy Melonio <mel.tammy2@gmail.com>
2020-04-03 16:17:01 -0300

    versão sem funções add em comentário

--- semana3/blackjack/desafio.js
+++ semana3/blackjack/desafio.js
@@ -91,3 +91,155 @@ function resultado() {
       alert("Empate!" + "\n" + textresult)
    } 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* **** Sem função *******
+let cartausuario = []
+let cartacpu = []
+let pontosusuario = []
+let pontoscpu = []
+let totalusuario = Number;
+let totalcpu = Number;
+console.log("Bem vindo ao jogo de Blackjack!")
+if (confirm("Quer iniciar uma nova rodada?")) {
+   let carta = comprarCarta();
+   cartausuario.push(carta.texto)
+   pontosusuario.push(carta.valor)
+   carta = comprarCarta();
+   cartausuario.push(carta.texto)
+   pontosusuario.push(carta.valor)
+   carta = comprarCarta();
+   cartacpu.push(carta.texto)
+   pontoscpu.push(carta.valor)
+   carta = comprarCarta();
+   cartacpu.push(carta.texto)
+   pontoscpu.push(carta.valor)
+   totalusuario = 0
+   totalcpu = 0
+   for (i = 0; i < pontosusuario.length; i++) {
+      totalusuario += pontosusuario[i];
+   }
+   for (i = 0; i < pontoscpu.length; i++) {
+      totalcpu += pontoscpu[i];
+   }
+   while ((totalusuario == 22) || (totalcpu == 22)) {
+      alert("As cartas serão sorteadas novamente")
+      let carta = comprarCarta();
+      cartausuario.push(carta.texto)
+      pontosusuario.push(carta.valor)
+      carta = comprarCarta();
+      cartausuario.push(carta.texto)
+      pontosusuario.push(carta.valor)
+      carta = comprarCarta();
+      cartacpu.push(carta.texto)
+      pontoscpu.push(carta.valor)
+      carta = comprarCarta();
+      cartacpu.push(carta.texto)
+      pontoscpu.push(carta.valor)
+      totalusuario = 0
+      totalcpu = 0
+      for (i = 0; i < pontosusuario.length; i++) {
+         totalusuario += pontosusuario[i];
+      }
+      for (i = 0; i < pontoscpu.length; i++) {
+         totalcpu += pontoscpu[i];
+      }
+   }
+   if (confirm("Suas cartas são " + cartausuario + ". A carta revelada do computador é " + cartacpu[0] + "\n" + "Deseja comprar mais uma carta?")) {
+      carta = comprarCarta();
+      cartausuario.push(carta.texto)
+      pontosusuario.push(carta.valor)
+      totalusuario = 0
+      totalcpu = 0
+      for (i = 0; i < pontosusuario.length; i++) {
+         totalusuario += pontosusuario[i];
+      }
+      for (i = 0; i < pontoscpu.length; i++) {
+         totalcpu += pontoscpu[i];
+      }
+      console.log("usuário comprou cartas")
+      while (totalusuario < 22) {
+         if (confirm("Suas cartas são " + cartausuario + ". A carta revelada do computador é " + cartacpu[0] + "\n" + "Deseja comprar mais uma carta?")) {
+            carta = comprarCarta();
+            cartausuario.push(carta.texto)
+            pontosusuario.push(carta.valor)
+            totalusuario = 0
+            totalcpu = 0
+            for (i = 0; i < pontosusuario.length; i++) {
+               totalusuario += pontosusuario[i];
+            }
+            for (i = 0; i < pontoscpu.length; i++) {
+               totalcpu += pontoscpu[i];
+            }
+            console.log("usuário comprou cartas")
+         } else{
+            totalusuario = 30;
+         }
+      }
+      
+      totalusuario = 0
+      totalcpu = 0
+      for (i = 0; i < pontosusuario.length; i++) {
+         totalusuario += pontosusuario[i];
+      }
+      for (i = 0; i < pontoscpu.length; i++) {
+         totalcpu += pontoscpu[i];
+      }
+   }
+}
+if (totalusuario < 21) {
+   while ((totalcpu < totalusuario) && (!(totalcpu == totalusuario))) {
+      carta = comprarCarta();
+      cartacpu.push(carta.texto)
+      pontoscpu.push(carta.valor)
+      totalusuario = 0
+      totalcpu = 0
+      for (i = 0; i < pontosusuario.length; i++) {
+         totalusuario += pontosusuario[i];
+      }
+      for (i = 0; i < pontoscpu.length; i++) {
+         totalcpu += pontoscpu[i];
+      }
+      console.log("cpu comprou cartas")
+   } 
+}
+console.log("Usuário - cartas: " + cartausuario + " - pontuação " + totalusuario);
+console.log("Computador - cartas: " + cartacpu + " - pontuação " + totalcpu)
+let textresult = ("Total: Usuário " + totalusuario + " X " + "Computador " + totalcpu + "\n" + "Cartas do usuário: " + cartausuario + "\n" + "Cartas do Computador: " + cartacpu);
+if (totalcpu > 21) {
+   alert("O usuário ganho!" + "\n" + textresult)
+} else if (totalusuario > 21) {
+   alert("O computador ganhou!" + "\n" + textresult)
+}
+if ((totalcpu !== totalusuario) && (totalcpu < 22) && (totalusuario < 22)) {
+   if (totalusuario > totalcpu) {
+      alert("O usuário ganhou!" + "\n" + textresult)
+   } else {
+      alert("O computador ganhou!" + "\n" + textresult)
+   }
+} else if (totalcpu == totalusuario) {
+   alert("Empate!" + "\n" + textresult)
+}
+ else {
+   console.log("O jogo acabou")
+}
+*/
