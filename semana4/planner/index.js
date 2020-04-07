b3384e864779cbb5b426ccdfa2e31b59632e7c24
Melissa Tammy Melonio <mel.tammy2@gmail.com>
2020-04-07 18:05:05 -0300

    Aula 14, desafio ok

--- semana4/planner/index.js
+++ semana4/planner/index.js
@@ -4,63 +4,81 @@ let quarta = document.getElementById("quartaTarefa")
 let quinta = document.getElementById("quintaTarefa")
 let sexta = document.getElementById("sextaTarefa")
 let sabado = document.getElementById("sabadoTarefa")
-let domingo = document.getElementById("DomingoTarefa")
+let domingo = document.getElementById("domingoTarefa")
 let tarefaInput = ""
 let diaTarefa = 0
 
 function criar(tarefaInput) {
     diaTarefa = Number(document.getElementById("dia").value)
-    console.log(diaTarefa)
-    switch (diaTarefa) {
-        case 2:
-            criarSegunda()
-            break;
-        case 3:
-            criarTerca()
-            break;
-        case 4:
-            criarQuarta()
-            break;
-        case 5:
-            criarQuinta()
-            break;
-        case 6:
-            criarSexta()
-            break;
-        case 7:
-            criarSabado()
-            break;
-        case 1:
-            criarDomingo()
-            break;
+    tarefaInput = document.getElementById("tarefaInput").value
+    if (tarefaInput == "") {
+        window.alert("insira uma tarefa")
+    } else {
+        console.log(diaTarefa)
+        switch (diaTarefa) {
+            case 2:
+                criarSegunda()
+                break;
+            case 3:
+                criarTerca()
+                break;
+            case 4:
+                criarQuarta()
+                break;
+            case 5:
+                criarQuinta()
+                break;
+            case 6:
+                criarSexta()
+                break;
+            case 7:
+                criarSabado()
+                break;
+            case 8:
+                criarDomingo()
+                break;
+        }
     }
     document.getElementById("tarefaInput").value = ""
-    
-}
 
+}
+function validacao(tarefaInput) {
+    tarefaInput = document.getElementById("tarefaInput").value
+    if (tarefaInput == "") {
+        window.alert("insira uma tarefa")
+    }
+}
 function criarSegunda(tarefaInput) {
     tarefaInput = document.getElementById("tarefaInput").value
-    segunda.innerHTML += "<p>" + tarefaInput + "</p>"
+    segunda.innerHTML += "<p onclick='riscar(this)'>" + tarefaInput + "</p>"
 }
 function criarTerca(tarefaInput) {
     tarefaInput = document.getElementById("tarefaInput").value
-    terca.innerHTML += "<p>" + tarefaInput + "</p>"
-} function criarQuarta(tarefaInput) {
+    terca.innerHTML += "<p onclick='riscar(this)'>" + tarefaInput + "</p>"
+} 
+function criarQuarta(tarefaInput) {
     tarefaInput = document.getElementById("tarefaInput").value
-    quarta.innerHTML += "<p>" + tarefaInput + "</p>"
-} function criarQuinta(tarefaInput) {
+    quarta.innerHTML += "<p onclick='riscar(this)'>" + tarefaInput + "</p>"
+} 
+function criarQuinta(tarefaInput) {
     tarefaInput = document.getElementById("tarefaInput").value
-    quinta.innerHTML += "<p>" + tarefaInput + "</p>"
-} function criarSexta(tarefaInput) {
+    quinta.innerHTML += "<p onclick='riscar(this)'>" + tarefaInput + "</p>"
+} 
+function criarSexta(tarefaInput) {
     tarefaInput = document.getElementById("tarefaInput").value
-    sexta.innerHTML += "<p>" + tarefaInput + "</p>"
-} function criarSabado(tarefaInput) {
+    sexta.innerHTML += "<p onclick='riscar(this)'>" + tarefaInput + "</p>"
+} 
+function criarSabado(tarefaInput) {
     tarefaInput = document.getElementById("tarefaInput").value
-    sabado.innerHTML += "<p>" + tarefaInput + "</p>"
-} function criarDomingo(tarefaInput) {
+    sabado.innerHTML += "<p onclick='riscar(this)'>" + tarefaInput + "</p>"
+} 
+function criarDomingo(tarefaInput) {
     tarefaInput = document.getElementById("tarefaInput").value
-    domingo.innerHTML += "<p>" + tarefaInput + "</p>"
+    domingo.innerHTML += "<p onclick='riscar(this)'>" + tarefaInput + "</p>"
+} 
+function riscar(paragrafo) {
+    paragrafo.style.textDecoration = "line-through";
 }
-// function (p){
-//     p.style.backgroundcolor = #55555;
-// }
+function limpar(){
+    location.reload()
+}
