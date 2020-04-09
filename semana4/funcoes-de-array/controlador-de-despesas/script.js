99d09d890d7c0f525a5d9ea3afe584e51ab7490b
Melissa Tammy Melonio <mel.tammy2@gmail.com>
2020-04-09 20:56:39 -0300

    2/3 completo

--- semana4/funcoes-de-array/controlador-de-despesas/script.js
+++ semana4/funcoes-de-array/controlador-de-despesas/script.js
@@ -1,18 +1,29 @@
 let novaDespesaNoExtrato = document.getElementById("container-extrato")
 let arrayDespesas = []
+let total = Number()
+
 function inserirDespesas() {
     const valor = document.getElementById("input-valor").value
     const tipo = document.getElementById("tipoDeDespesa").value
     const descricao = document.getElementById("textAreaDescricao").value
     const despesa = {
-        valor: valor,
+        valor: Math.trunc(valor),
         tipo: tipo,
         descricao: descricao
     }
     arrayDespesas.push(despesa)
+    novaDespesaNoExtrato.innerHTML += "<p> R$" + despesa.valor+ " - " + despesa.tipo + "</p>"
+    total += Number(despesa.valor)
+    document.getElementById("resultadoFinal").innerHTML = "<p>"+total+"</p>" 
+    const apenasCasa = arrayDespesas.filter((tipoDespesa, index, array) => {
+        if(tipoDespesa.tipo === "Despesas de casa"){
+            document.getElementById("container-filtro").innerHTML = "<p>"+ arrayDespesas  +"</p>"
+        }
+    })
 }
-console.log(arrayDespesas)
 
-for (i = 0; i < arrayDespesas.length; i++) {
-    novaDespesaNoExtrato.innerHTML += "<p>" + "oi" + "</p>"
-}
+// const apenasCasa = arrayDespesas.filter((tipoDespesa, index, array) => {
+//     if(tipoDespesa.tipo === "Despesas de casa"){
+//         document.getElementById("container-filtro").innerHTML = "<p>"+ arrayDespesas[index].  +"</p>"
+//     }
+// })
