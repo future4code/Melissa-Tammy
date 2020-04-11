aa8416c05321407375f9af0a526f77c05a5dea26
Melissa Tammy Melonio <mel.tammy2@gmail.com>
2020-04-11 17:15:05 -0300

    Objetos- ex1

--- semana4/lista-de-exercicios/script-aula15.js
+++ semana4/lista-de-exercicios/script-aula15.js
@@ -45,3 +45,18 @@ let alerta = function () {
 }
 alerta()
 */
+
+/*___________E X E R C Í C I O S - O B J E T O S______________________________*
+1. As arrays são semelhantes as listas, armazenam itens (seja objeto, variavel,
+numero etc) que podem ser acessados pela seguinte sintaxe: nomeDaArray[i], sendo 
+i um numero inteiro que representa a posição do item dentro da array; pode ser
+utilizada quando queremos agrupar dados que possuem classificação/tipo semelhante,
+como por exemplo: animais=["urso", "sapo", "vira lata caramelo"].
+Os objetos são como blocos de dados que juntos formam uma informação, pode possuir 
+classe, atributos e métodos. Os atributos são identificados com uma chave e um 
+valor. Os objetos se diferem das arrays quando observamos o agrupamento dos dados,
+os itens de uma array não tem relações diretas (exceto as posições), já nos objetos,
+cada atributo faz parte do objeto e depende da sua existência para fazer sentido 
+como informação. Podem ser usados quando os dados devem ser agrupados e ligados a 
+uma entidade, exemplo: classe: pessoas, objeto: Melissa{nome: Melissa TM, idade:21}.
+*/
