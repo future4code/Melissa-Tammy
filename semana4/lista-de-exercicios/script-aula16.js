1319e15f88f7bc0267958ade5c5afa15282aed58
Melissa Tammy Melonio <mel.tammy2@gmail.com>
2020-04-11 18:45:57 -0300

    Aula16, ex4

--- semana4/lista-de-exercicios/script-aula16.js
+++ semana4/lista-de-exercicios/script-aula16.js
@@ -78,3 +78,26 @@ console.log(funcaoMultiplica3(array))
 console.log(funcaoString(array))
 */
 
+/************ EXERCÍCIO 4 ************
+const pessoas = [
+    { nome: "Paula", idade: 12, altura: 1.8 },
+    { nome: "João", idade: 20, altura: 1.3 },
+    { nome: "Pedro", idade: 15, altura: 1.9 },
+    { nome: "Luciano", idade: 22, altura: 1.8 },
+    { nome: "Artur", idade: 10, altura: 1.2 },
+    { nome: "Soter", idade: 70, altura: 1.9 }
+]
+let permitido = []
+let npermitido = []
+function permissao(array){
+    array.forEach((pessoa, index, arr) => {
+        if ((pessoa.altura >= 1.5)&&(pessoa.idade >= 14)&&(pessoa.idade < 60)){
+            permitido.push(pessoa)
+        } else {
+            npermitido.push(pessoa)
+        }
+    })
+    console.log(permitido, npermitido)
+}
+permissao(pessoas)
+*/
