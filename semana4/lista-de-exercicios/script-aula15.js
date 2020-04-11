a164280c488f88d8e78722a107043ed0d07eb431
Melissa Tammy Melonio <mel.tammy2@gmail.com>
2020-04-11 16:48:16 -0300

    Aula 15, Função, ex1

---
+++ semana4/lista-de-exercicios/script-aula15.js
@@ -0,0 +1,38 @@
+/*___________E X E R C Í C I O S - F U N Ç Õ E S______________________________*/
+
+/************ EXERCÍCIO 1 ************
+function funcaoArray(array) {
+    let maiorNumero = Infinity;
+    let menorNumero = 0;
+    for (let numero of array) {
+        if (numero < maiorNumero) {
+            maiorNumero = numero
+        }
+        if (numero > menorNumero) {
+            menorNumero = numero;
+        }
+    }
+    console.log(array, menorNumero, maiorNumero)
+    let segundoMenorNumero = 0;
+    let segundoMaiorNumero = Infinity;
+    for (let numero of array) {
+        if (numero !== maiorNumero) {
+            if (numero < segundoMaiorNumero) {
+                segundoMaiorNumero = numero
+            }
+        }
+        if (numero !== menorNumero) {
+            if (numero > segundoMenorNumero) {
+                segundoMenorNumero = numero;
+            }
+        }
+    }
+    console.log(array, segundoMenorNumero, segundoMaiorNumero)
+
+
+}
+let array = []
+for (i = 0; i < 21; i++) {
+    array.push(Math.floor(Math.random() * 100))
+}
+funcaoArray(array)*/
