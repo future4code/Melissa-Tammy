f0546d6fd2a90a1f341b5675075e4caf30f52b83
Melissa Tammy Melonio <mel.tammy2@gmail.com>
2020-04-10 22:08:44 -0300

    leitura - ex1

--- semana4/lista-de-exercicios/script.js
+++ semana4/lista-de-exercicios/script.js
@@ -1,5 +1,9 @@
+/*___________L E I T U R A     D E      C Ó D I G O______________________________*/
+
 /************ EXERCÍCIO 1 ************
 A função faz conversão de dolares de acordo com a cotação inserida pelo usuário,
 multiplicando a quantidade de dolar pela cotação e retornando o valor em real.
-O valor impresso no console tendo 100 como parâmetro da função, será "R$100*<cotacao inserida pelo usuário>"
+O valor impresso no console tendo 100 como parâmetro da função, será "R$100*
+<cotacao inserida pelo usuário>"
 */
+
