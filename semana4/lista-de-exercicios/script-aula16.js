2eb287dfe7e9575e0c04aab6ea6674a4522c6123
Melissa Tammy Melonio <mel.tammy2@gmail.com>
2020-04-11 19:43:21 -0300

    Aula16, ex6

--- semana4/lista-de-exercicios/script-aula16.js
+++ semana4/lista-de-exercicios/script-aula16.js
@@ -146,4 +146,19 @@ mensagem(consultas)
 console.log(arrayCancelado, arrayMarcado)
 */
 
-
+/************ EXERCÍCIO 5 ************
+const contas = [
+    { cliente: "João", saldoTotal: 1000, compras: [100, 200, 300] },
+    { cliente: "Paula", saldoTotal: 7500, compras: [200, 1040] },
+    { cliente: "Pedro", saldoTotal: 10000, compras: [5140, 6100, 100, 2000] },
+    { cliente: "Luciano", saldoTotal: 100, compras: [100, 200, 1700] },
+    { cliente: "Artur", saldoTotal: 1800, compras: [200, 300] },
+    { cliente: "Soter", saldoTotal: 1200, compras: [] }
+]
+contas.forEach((pessoa, index, array) => {
+    for (i = 0; i < pessoa.compras.length; i++) {
+        pessoa.saldoTotal -= pessoa.compras[i]       
+    }
+})
+console.log(contas)
+*/
