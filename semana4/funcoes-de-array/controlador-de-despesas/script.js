0dcfe602aab83cfa3449d4238b167d49e032edba
Melissa Tammy Melonio <mel.tammy2@gmail.com>
2020-04-13 00:31:17 -0300

    exercío da tarde completo, sem desafio

--- semana4/funcoes-de-array/controlador-de-despesas/script.js
+++ semana4/funcoes-de-array/controlador-de-despesas/script.js
@@ -1,29 +1,69 @@
 let novaDespesaNoExtrato = document.getElementById("container-extrato")
 let arrayDespesas = []
 let total = Number()
-
 function inserirDespesas() {
-    const valor = document.getElementById("input-valor").value
-    const tipo = document.getElementById("tipoDeDespesa").value
-    const descricao = document.getElementById("textAreaDescricao").value
-    const despesa = {
-        valor: Math.trunc(valor),
-        tipo: tipo,
-        descricao: descricao
-    }
-    arrayDespesas.push(despesa)
-    novaDespesaNoExtrato.innerHTML += "<p> R$" + despesa.valor+ " - " + despesa.tipo + "</p>"
-    total += Number(despesa.valor)
-    document.getElementById("resultadoFinal").innerHTML = "<p>"+total+"</p>" 
-    const apenasCasa = arrayDespesas.filter((tipoDespesa, index, array) => {
-        if(tipoDespesa.tipo === "Despesas de casa"){
-            document.getElementById("container-filtro").innerHTML = "<p>"+ arrayDespesas  +"</p>"
+    let valor = document.getElementById("input-valor").value
+    let tipo = document.getElementById("tipoDeDespesa").value
+    let descricao = document.getElementById("textAreaDescricao").value
+    let auxValor = valor.indexOf(".")
+    if ((valor !== "") && (tipo !== "") && (descricao !== "")&&(auxValor === -1)) {
+        let despesa = {
+            valor: Math.trunc(valor),
+            tipo: tipo,
+            descricao: descricao
         }
-    })
+        arrayDespesas.push(despesa)
+        let arrayDespesasJson = JSON.stringify(arrayDespesas)
+        localStorage.setItem("arrayDespesaJson", arrayDespesasJson)
+        total += Number(despesa.valor)
+        let arrayDespesasConvertido = JSON.parse(localStorage.getItem("arrayDespesaJson"))
+        novaDespesaNoExtrato.innerHTML = ""
+        arrayDespesasConvertido.forEach((despesa, index, array) => {
+            novaDespesaNoExtrato.innerHTML += `<p>${despesa.valor} - ${despesa.tipo}</p>`
+        })
+        document.getElementById("resultadoFinal").innerHTML = "<p>" + total + "</p>"
+        document.getElementById("input-valor").value = ""
+        document.getElementById("tipoDeDespesa").value = ""
+        document.getElementById("textAreaDescricao").value = ""
+    } else {
+        window.alert("dados invalidos")
+    }
 }
 
-// const apenasCasa = arrayDespesas.filter((tipoDespesa, index, array) => {
-//     if(tipoDespesa.tipo === "Despesas de casa"){
-//         document.getElementById("container-filtro").innerHTML = "<p>"+ arrayDespesas[index].  +"</p>"
-//     }
-// })
+function funcaoFiltrar() {
+    let localDespesasDetalhadas = document.getElementById("container-filtro")
+    let optionDespesa = document.getElementById("tipoDaDespesa").value
+    let arrayDespesasConvertido = JSON.parse(localStorage.getItem("arrayDespesaJson"))
+    switch (optionDespesa) {
+        case "Todas Despesas":
+            localDespesasDetalhadas.innerHTML = ""
+            arrayDespesasConvertido.forEach((despesa, index, array) => {
+                localDespesasDetalhadas.innerHTML += `<p>${despesa.valor} - ${despesa.descricao} - ${despesa.tipo}</p>`
+            })
+            break;
+        case "Despesas de casa":
+            localDespesasDetalhadas.innerHTML = ""
+            arrayDespesasConvertido.filter(despesa => {
+                if (despesa.tipo === "Despesas de casa") {
+                    localDespesasDetalhadas.innerHTML += `<p>${despesa.valor} - ${despesa.descricao} - ${despesa.tipo}</p>`
+                }
+            })
+            break;
+        case "Despesas de Alimentacao":
+            localDespesasDetalhadas.innerHTML = ""
+            arrayDespesasConvertido.filter(despesa => {
+                if (despesa.tipo === "Despesas de Alimentacao") {
+                    localDespesasDetalhadas.innerHTML += `<p>${despesa.valor} - ${despesa.descricao} - ${despesa.tipo}</p>`
+                }
+            })
+            break;
+        case "Despesas de Transporte":
+            localDespesasDetalhadas.innerHTML = ""
+            arrayDespesasConvertido.filter(despesa => {
+                if (despesa.tipo === "Despesas de Transporte") {
+                    localDespesasDetalhadas.innerHTML += `<p>${despesa.valor} - ${despesa.descricao} - ${despesa.tipo}</p>`
+                }
+            })
+            break;
+    }
+}
