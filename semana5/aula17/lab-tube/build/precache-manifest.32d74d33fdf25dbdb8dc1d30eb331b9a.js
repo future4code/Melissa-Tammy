aa3bf890573e8f31d9cfe232a5dc2ea2e2c2fa62
Your Name <mel.tammy2@gmail.com>
2020-04-14 17:02:41 -0300

    aula18

---
+++ semana5/aula17/lab-tube/build/precache-manifest.32d74d33fdf25dbdb8dc1d30eb331b9a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f9772d72f91edef8ef658c4c30ed853",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c6b00449f302619ef9d0",
+    "url": "/static/css/main.94dead48.chunk.css"
+  },
+  {
+    "revision": "6c72f9f5e84e9eee28ec",
+    "url": "/static/js/2.113281db.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.113281db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6b00449f302619ef9d0",
+    "url": "/static/js/main.56555c0e.chunk.js"
+  },
+  {
+    "revision": "86eeef2b77b4a541b6ec",
+    "url": "/static/js/runtime-main.be69ecd0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
