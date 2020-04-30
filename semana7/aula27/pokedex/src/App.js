dd2f6099db73612e52fcf33fd4bcbf3f8ea14d51
Melissa Tammy <mel.tammy2@gmail.com>
2020-04-30 19:57:49 -0300

    Finalizado

--- semana7/aula27/pokedex/src/App.js
+++ semana7/aula27/pokedex/src/App.js
@@ -3,13 +3,13 @@ import './App.css';
 import PokebolaFechada from './components/PokebolaFechada'
 import Pokemons from './components/Pokemons'
 import styled from 'styled-components';
+import axios from 'axios'
 
 const Container = styled.div`
   max-width:100vw;
   display:flex;
   flex-direction: column;
 `
-
 const ContainerPokebolaAberta = styled.div`
   max-width:100vw;
   display:flex;
@@ -18,22 +18,22 @@ const ContainerPokebolaAberta = styled.div`
   align-items:center;
   min-height:100vh;
 `
-
 const NavBar = styled.div`
   background-color: red;
   border-bottom: 2vw black solid;
   height: 8vw;
-  width:100vw;
+  width:100%;
   display: flex;
   justify-content: center;
+
 `
 const Footer = styled.div`
   background-color: white;
   border-top: 2vw black solid;
   height: 5vw;
-  width:100vw;
-`
+  width:100%;
 
+`
 const BotaoBokebola = styled.button`
   border-radius: 100vw;
   width: 15vw;
@@ -50,31 +50,9 @@ const BotaoBokebola = styled.button`
 
 class App extends React.Component {
   state = {
-    listaPokemons:[ {
-      "name": "bulbasaur",
-      "url": "https://pokeapi.co/api/v2/pokemon/1/"
-  },
-  {
-      "name": "ivysaur",
-      "url": "https://pokeapi.co/api/v2/pokemon/2/"
-  },
-  {
-      "name": "venusaur",
-      "url": "https://pokeapi.co/api/v2/pokemon/3/"
-  },
-  {
-      "name": "charmander",
-      "url": "https://pokeapi.co/api/v2/pokemon/4/"
-  },
-  {
-      "name": "charmeleon",
-      "url": "https://pokeapi.co/api/v2/pokemon/5/"
-  },
-  {
-      "name": "charizard",
-      "url": "https://pokeapi.co/api/v2/pokemon/6/"
-  }],
-    abrePokebola: true
+    listaNomes: [],
+    listaPokemons:[],
+    abrePokebola: false
   }
 
   onClickBotaoPokebola = () => {
@@ -83,22 +61,63 @@ class App extends React.Component {
     })
   }
 
+  componentDidMount = () => {
+    console.log("entrou do did mount")
+    axios.get("https://pokeapi.co/api/v2/pokemon/?limit=151")
+      .then((response) => {
+        console.log("antes set state Nome")
+        this.setState({
+          listaNomes: response.data.results
+        })
+        console.log("depois set state Nome")
+        {
+          this.state.listaNomes.map((poke) => {
+            console.log("Entrou no map")
+            axios
+              .get(`https://pokeapi.co/api/v2/pokemon/${poke.name}`)
+              .then((response) => {
+                
+                const auxPokemon = {
+                  nome: response.data.name,
+                  foto: response.data.sprites.front_default
+                }
+                const arrayAux = [auxPokemon, ...this.state.listaPokemons];
+                this.setState({
+                  listaPokemons: arrayAux
+                })
+                console.log(`Deu certo o get, o nome é ${response.data.name} e a url é ${response.data.sprites.front_default}`)
+                console.log(this.state.listaPokemons)
+              }).catch((error) => {
+                console.log(error.response)
+              })
+            return (
+              <div />
+            )
+          })
+        }
+      }).catch((error) => {
+        console.log(error.response)
+      })
+  }
+
+
+
+
   render() {
     return (
       <Container>
         {this.state.abrePokebola ? (
           <ContainerPokebolaAberta>
             <NavBar>
-            <BotaoBokebola onClick={this.onClickBotaoPokebola}/>
+              <BotaoBokebola onClick={this.onClickBotaoPokebola} />
             </NavBar>
             <Pokemons pegaListaPokemon={this.state.listaPokemons}>
 
             </Pokemons>
-            <h1>Testeee</h1>
             <Footer></Footer>
           </ContainerPokebolaAberta>
         ) : (
-          <PokebolaFechada onClickBotaoPokebolaApp={this.onClickBotaoPokebola}/>
+            <PokebolaFechada onClickBotaoPokebolaApp={this.onClickBotaoPokebola} />
           )}
       </Container>
     );
--- semana7/aula27/pokedex/src/components/Pokemons.js
+++ semana7/aula27/pokedex/src/components/Pokemons.js
@@ -2,33 +2,51 @@ import React from 'react';
 import styled from 'styled-components';
 
 const Container = styled.div`
-  max-width:100vw;
   display:flex;
-  justify-content: center;
+  align-items: center;
+  margin-top:7.0vw;
+  padding-left:0;
+
+`
+
+const CardPokemon = styled.div`
+    display: flex;
+    width: 25%;
+    flex-direction: column;
+    border: 1px black solid;
+    border-radius: 5px;
+    margin: 1vw;
+    justify-content: center;
+    align-items: center;
 `
 
 const ContainerPokemon = styled.div`
     display: flex;
-    flex-direction: column;
-    border: 1px black solid;
+    flex-wrap: wrap;
+    justify-content: center;
+`
+
+const Nome = styled.h1`
+    font-size:1em;
 `
 
 class Pokemons extends React.Component {
     render() {
-
         const imprimePokemons = this.props.pegaListaPokemon.map((poke) => {
-            return(
-                <ContainerPokemon>
-                    <h3>{poke.name}</h3>
-                    <img src={poke.url}/>
-                </ContainerPokemon>
+            return (
+                <CardPokemon>
+                    <Nome>{poke.nome}</Nome>
+                    <img src={poke.foto} />
+                </CardPokemon>
             )
         })
 
         return (
             <Container>
-                {imprimePokemons}
-            </Container>
+                <ContainerPokemon>
+                    {imprimePokemons}
+                </ContainerPokemon>
+            </Container >
 
         );
     }
