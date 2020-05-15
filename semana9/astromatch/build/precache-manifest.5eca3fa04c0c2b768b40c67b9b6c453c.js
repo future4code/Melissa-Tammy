e8416bf1eee410ea60cc00ab6fe4798ba4dda3ab
Melissa Tammy <mel.tammy2@gmail.com>
2020-05-15 14:02:39 -0300

    tentando alterar o nome da pasta

---
+++ semana9/astromatch/build/precache-manifest.5eca3fa04c0c2b768b40c67b9b6c453c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6437c09b26f09afdefaeaaca1daeabf0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "51f318183e7e4d68859e",
+    "url": "/static/css/main.1ba88f36.chunk.css"
+  },
+  {
+    "revision": "d0f74da8e121aef7ff2b",
+    "url": "/static/js/2.000730a0.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.000730a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51f318183e7e4d68859e",
+    "url": "/static/js/main.8d788a16.chunk.js"
+  },
+  {
+    "revision": "7669822dc692f6c1b75b",
+    "url": "/static/js/runtime-main.5900f00a.js"
+  }
+]);
