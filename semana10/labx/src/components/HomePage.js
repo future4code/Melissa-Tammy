1bd16612b018266b851a2d5dc1c3707bfbb9f1b3
Melissa Tammy <mel.tammy2@gmail.com>
2020-05-18 15:42:52 -0300

    componentes criados

---
+++ semana10/labx/src/components/HomePage.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from 'react';
+import axios from "axios"
+import styled from 'styled-components'
+
+const HomePage = (props) => {
+    return (
+      <div>
+  
+      </div>
+    );
+  }
+  export default HomePage;
