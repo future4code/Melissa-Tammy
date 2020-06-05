16a7734e9b7ebcc43b6c5c6af1603f4626b72d55
Melissa Tammy <mel.tammy2@gmail.com>
2020-06-05 15:25:27 -0300

    layout e funcionalidades+integração com API finalizados, agora falta os testes

--- semana11/planner/src/App.js
+++ semana11/planner/src/App.js
@@ -4,48 +4,110 @@ import axios from "axios";
 
 import './App.css';
 
+const Tarefa = styled.p`
+  text-align: left;
+  text-decoration: ${({ completa }) => (completa ? 'line-through' : 'none')};
+`
+
 const App = (props) => {
   const [listaTarefas, setListaTarefas] = useState([])
   const [novaTarefa, setNovaTarefa] = useState('')
-
+  const [dia, setDia] = useState('segunda')
   useEffect(() => {
     pegaTarefa()
-  }, []);
+  }, [listaTarefas]);
 
   const pegaTarefa = () => {
     axios.get('https://us-central1-labenu-apis.cloudfunctions.net/generic/planner-julian-meltammy')
-    .then(response => {
-      setListaTarefas(response.data)
-    })
-    .catch(error => {
-      alert(error)
-    })
+      .then(response => {
+        setListaTarefas(response.data)
+      })
+      .catch(error => {
+        alert(error)
+      })
   }
 
-  const onChangeInputTarefa = event => {
-    setNovaTarefa(event.target)
+  const onChangeInputTarefa = (event) => {
+    setNovaTarefa(event.target.value)
   }
 
+  const onChangeSelectDia = (event) => {
+    setDia(event.target.value)
+  }
+
+  const criarTarefa = () => {
+    const body = {
+      text: novaTarefa,
+      day: dia,
+      completa: false
+    }
+
+    if (novaTarefa !== '') {
+      axios
+        .post('https://us-central1-labenu-apis.cloudfunctions.net/generic/planner-julian-meltammy',
+          body,
+          {
+            headers: {
+              'Content-Type': 'application/json'
+            }
+          }
+        )
+        .then((response) => {
+          alert("Tarefa inserida")
+          pegaTarefa()
+        })
+        .catch((error) => {
+          alert(error)
+        })
+    }
+
+  }
+
+  const alteraStatus = (id, status) => {
+    const body = {
+      "completa": !status
+    }
+    axios
+      .put(`https://us-central1-labenu-apis.cloudfunctions.net/generic/planner-julian-meltammy/${id}`,
+        body)
+      .then((response) => {
+        console.log("acionou o alterastatus")
+      })
+      .catch((error) => {
+        alert(error)
+      })
+  }
+
+  const apagaTarefa = (id) => {
+    axios
+      .delete(`https://us-central1-labenu-apis.cloudfunctions.net/generic/planner-julian-meltammy/${id}`)
+      .then((response) => {
+        console.log("acionou o apaga tarefa")
+      })
+      .catch((error) => {
+        alert(error)
+      })
+  }
 
   return (
     <div className="container">
       <header>
         <div className="dia-semana-input">
           <label> Selecione o dia da semana: </label>
-          <select>
-            <option value="segunda">Segunda</option>
-            <option value="terca">Terça</option>
-            <option value="quarta">Quarta</option>
-            <option value="quinta">Quinta</option>
-            <option value="sexta">Sexta</option>
-            <option value="sabado">Sábado</option>
-            <option value="domingo">Domingo</option>
+          <select onChange={onChangeSelectDia}>
+            <option value={"segunda"}>Segunda</option>
+            <option value={"terca"}>Terça</option>
+            <option value={"quarta"}>Quarta</option>
+            <option value={"quinta"}>Quinta</option>
+            <option value={"sexta"}>Sexta</option>
+            <option value={"sabado"}>Sábado</option>
+            <option value={"domingo"}>Domingo</option>
           </select>
         </div>
 
         <div className="container-input">
-          <input type="text" placeholder="Nova tarefa" value={novaTarefa} onChange={onChangeInputTarefa()} />
-          <button onclick="criarTarefa()">Criar</button>
+          <input type="text" placeholder="Nova tarefa" value={novaTarefa} onChange={onChangeInputTarefa} />
+          <button onClick={criarTarefa}>Criar</button>
         </div>
       </header>
       <div className="main-container">
@@ -53,9 +115,14 @@ const App = (props) => {
           <h1>Segunda</h1>
           <article id="segunda">
             {listaTarefas.map((tarefa) => {
-              if (tarefa.day == 'Segunda') {
+              if (tarefa.day == 'segunda') {
                 return (
-                  <p>{tarefa.text}</p>
+                  <Tarefa
+                    completa={tarefa.completa}
+                    onClick={() => alteraStatus(tarefa.id, tarefa.completa)}
+                    onDoubleClick={() => apagaTarefa(tarefa.id)}>
+                    {tarefa.text}
+                  </Tarefa>
                 )
               }
             })}
@@ -65,9 +132,14 @@ const App = (props) => {
           <h1>Terça</h1>
           <article id="terca">
             {listaTarefas.map((tarefa) => {
-              if (tarefa.day == 'Terca') {
+              if (tarefa.day == 'terca') {
                 return (
-                  <p>{tarefa.text}</p>
+                  <Tarefa
+                    completa={tarefa.completa}
+                    onClick={() => alteraStatus(tarefa.id, tarefa.completa)}
+                    onDoubleClick={() => apagaTarefa(tarefa.id)}>
+                    {tarefa.text}
+                  </Tarefa>
                 )
               }
             })}
@@ -79,7 +151,12 @@ const App = (props) => {
             {listaTarefas.map((tarefa) => {
               if (tarefa.day == 'quarta') {
                 return (
-                  <p>{tarefa.text}</p>
+                  <Tarefa
+                    completa={tarefa.completa}
+                    onClick={() => alteraStatus(tarefa.id, tarefa.completa)}
+                    onDoubleClick={() => apagaTarefa(tarefa.id)}>
+                    {tarefa.text}
+                  </Tarefa>
                 )
               }
             })}
@@ -91,7 +168,12 @@ const App = (props) => {
             {listaTarefas.map((tarefa) => {
               if (tarefa.day == 'quinta') {
                 return (
-                  <p>{tarefa.text}</p>
+                  <Tarefa
+                    completa={tarefa.completa}
+                    onClick={() => alteraStatus(tarefa.id, tarefa.completa)}
+                    onDoubleClick={() => apagaTarefa(tarefa.id)}>
+                    {tarefa.text}
+                  </Tarefa>
                 )
               }
             })}
@@ -103,7 +185,12 @@ const App = (props) => {
             {listaTarefas.map((tarefa) => {
               if (tarefa.day == 'sexta') {
                 return (
-                  <p>{tarefa.text}</p>
+                  <Tarefa
+                    completa={tarefa.completa}
+                    onClick={() => alteraStatus(tarefa.id, tarefa.completa)}
+                    onDoubleClick={() => apagaTarefa(tarefa.id)}>
+                    {tarefa.text}
+                  </Tarefa>
                 )
               }
             })}
@@ -115,7 +202,12 @@ const App = (props) => {
             {listaTarefas.map((tarefa) => {
               if (tarefa.day == 'sabado') {
                 return (
-                  <p>{tarefa.text}</p>
+                  <Tarefa
+                    completa={tarefa.completa}
+                    onClick={() => alteraStatus(tarefa.id, tarefa.completa)}
+                    onDoubleClick={() => apagaTarefa(tarefa.id)}>
+                    {tarefa.text}
+                  </Tarefa>
                 )
               }
             })}
@@ -127,16 +219,18 @@ const App = (props) => {
             {listaTarefas.map((tarefa) => {
               if (tarefa.day == 'domingo') {
                 return (
-                  <p>{tarefa.text}</p>
+                  <Tarefa
+                    completa={tarefa.completa}
+                    onClick={() => alteraStatus(tarefa.id, tarefa.completa)}
+                    onDoubleClick={() => apagaTarefa(tarefa.id)}>
+                    {tarefa.text}
+                  </Tarefa>
                 )
               }
             })}
           </article>
         </div>
       </div>
-      <footer>
-        <button onclick="limpar()">Limpar</button>
-      </footer>
     </div>
   );
 }
