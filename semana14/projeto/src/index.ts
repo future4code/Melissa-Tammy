5d31a52a21dc193d708279c819c46a53489c5069
Melissa Tammy <mel.tammy2@gmail.com>
2020-06-27 17:45:32 -0300

    adicionar saldo ok

--- semana14/projeto/src/index.ts
+++ semana14/projeto/src/index.ts
@@ -37,34 +37,60 @@ function criarUsuario(novoUsuario: usuario): void {
 }
 
 switch (opcao) {
+    case 0:
+        console.log(users)
+        break;
     case 1:
         let novaTransacao: transacao = {
-            data: "12-45-63",
+            data: "data",
             descricao: "Add money",
-            valor: 50
+            valor: Number(process.argv[4])
         }
         users.map(us => {
             if (us.cpf == process.argv[3]) {
-                us.conta!.push(novaTransacao)
+                us.conta.push(novaTransacao)
                 const userStringfied = JSON.stringify(users, null, 2)
                 fs.writeFileSync("users.json", userStringfied)
             }
         })
-        case 2:
-            let nomeInserido: string = process.argv[3]
-            let cpfInserido: string = process.argv[4]
-            let dtNascInserido: string = process.argv[5]
-            let newUser: usuario = {
-                nome: nomeInserido,
-                cpf: cpfInserido,
-                dtNasc: dtNascInserido,
-                conta: []
+        break;
+    case 2:
+        let nomeInserido: string = process.argv[3]
+        let cpfInserido: string = process.argv[4]
+        let dtNascInserido: string = process.argv[5]
+        let newUser: usuario = {
+            nome: nomeInserido,
+            cpf: cpfInserido,
+            dtNasc: dtNascInserido,
+            conta: []
+        }
+        try {
+            console.log(newUser)
+            criarUsuario(newUser)
+        } catch{
+            console.log("erro")
+        }
+        break;
+    case 3:
+        users.map(us => {
+            if (us.cpf == process.argv[3] && us.conta !== []) {
+                let transacoes: transacao[] = us.conta
+                transacoes.map(tran => {
+                    console.log("Data: ", tran.data, "\nDescrição: ", tran.descricao, "\nvalor: R$", tran.valor)
+                })
             }
-            try {
-                console.log(newUser)
-                criarUsuario(newUser)
-            } catch{
-                console.log("erro")
+        })
+        break;
+    case 4:
+        users.map(us => {
+            if (us.cpf == process.argv[3]) {
+                let transacoes: transacao[] = us.conta
+                let saldo: number = 0
+                transacoes.map(tran => {
+                    saldo += tran.valor
+                })
+                return console.log("saldo: R$", saldo)
             }
-            break;
+        })
+        break;
 }
