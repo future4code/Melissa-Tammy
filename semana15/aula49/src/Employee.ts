8e1fda49f7d7b7f8abeaf9ec0e652e54632b4760
Melissa Tammy <mel.tammy2@gmail.com>
2020-06-30 20:57:20 -0300

    ex7

---
+++ semana15/aula49/src/Employee.ts
@@ -0,0 +1,32 @@
+import { User } from './user'
+
+export class Employee extends User {
+    protected admissionDate: Date;
+    protected baseSalary: number;
+
+    constructor(
+        id: string,
+        email: string,
+        name: string,
+        password: string,
+        admissionDate: Date,
+        baseSalary: number
+    ) {
+        super(id, email, name, password);
+        this.admissionDate = admissionDate;
+        this.baseSalary = baseSalary;
+    }
+
+    public getAdmissionDate(): Date {
+        return this.admissionDate
+    }
+
+    public getBaseSalary(): number {
+        return this.baseSalary
+    }
+
+    public calculateTotalSalary(): number{
+        return this.baseSalary+400
+    }
+}
+
--- semana15/aula49/src/index.ts
+++ semana15/aula49/src/index.ts
@@ -1,5 +1,7 @@
 import { User } from './user'
 import { Customer } from './Customer'
+import { Employee } from './Employee'
+
 type person = {
   name: string
 }
@@ -14,8 +16,9 @@ function createUser(id: string, email: string, name: string, password: string, c
 
 //const myPerson2 = createPerson("Robson");
 //const myPerson3: User = new User(`${Date.now()}`, "mel@gmail.com ", "Mel", "13256")
-const myPerson4: Customer = new Customer(`${Date.now()}`, "mel@gmail.com ", "Mel", "13256", "cartão")
-console.log(myPerson4.introduceYourself());
+//const myPerson4: Customer = new Customer(`${Date.now()}`, "mel@gmail.com ", "Mel", "13256", "cartão")
+const myPerson5: Employee = new Employee(`${Date.now()}`, "mel@gmail.com ", "Mel", "13256", new Date(2020, 11, 1), 1500)
+console.log(myPerson5.calculateTotalSalary());
 
 
 /*_______________E X E R C Í C I O 1 _______________
@@ -43,4 +46,15 @@ a. Seria possível imprimir a senha (password) atrelada a essa instância? Por q
 
 /*_______________E X E R C Í C I O 5 _______________
   Linha 36 do './user.ts'
+*/
+
+/*_______________E X E R C Í C I O 6 _______________
+a. Quantas vezes a mensagem "Chamando o construtor da classe User" foi impressa no terminal?
+  Somente uma vez.
+b. Imprima as informações dessa instância no terminal. Quais dados são possíveis de serem impressos?
+  Todos.
+*/
+
+/*_______________E X E R C Í C I O 7 _______________
+  Linha 28 a 31 './Employee.ts'
 */
