7adf3aa69de96ae59a75f1bfcd546a1c9f95a01f
Melissa Tammy <mel.tammy2@gmail.com>
2020-06-30 20:24:45 -0300

    ex2

--- semana15/aula49/src/index.ts
+++ semana15/aula49/src/index.ts
@@ -1,5 +1,5 @@
 import { User } from './user'
-
+import { Customer } from './Customer'
 type person = {
   name: string
 }
@@ -8,10 +8,26 @@ function createPerson(name: string): person {
   return { name: name }
 }
 
-function createUser(id: string, email: string, name: string, password: string){
-  
+function createUser(id: string, email: string, name: string, password: string, creditCard: string) {
+
 }
 
-const myPerson2 = createPerson("Robson");
-const myPerson3: User = new User(`${Date.now()}`, "mel@gmail.com ", "Mel", "13256")
-console.log(myPerson3);
+//const myPerson2 = createPerson("Robson");
+//const myPerson3: User = new User(`${Date.now()}`, "mel@gmail.com ", "Mel", "13256")
+const myPerson4: Customer = new Customer(`${Date.now()}`, "mel@gmail.com ", "Mel", "13256", "cartão")
+console.log(myPerson4);
+
+
+/*_______________E X E R C Í C I O 1 _______________
+a. Seria possível imprimir a senha (password) atrelada a essa instância?
+  Não, pois não9 há o método get.
+b. Quantas vezes a mensagem "Chamando o construtor da classe User" foi impressa no terminal?
+  Somente uma vez.
+*/
+
+/*_______________E X E R C Í C I O 2 _______________
+a. Quantas vezes a mensagem "Chamando o construtor da classe Customer" foi impressa no terminal?
+  Somente uma vez.
+b. Quantas vezes a mensagem "Chamando o construtor da classe User" foi impressa no terminal? Por quê?
+  Somente uma vez, pois a classe Customer herda User.
+*/
