e294d02dcf6a35821c1dc2790d57d77543841a88
Melissa Tammy <mel.tammy2@gmail.com>
2020-07-01 16:17:07 -0300

    ex3

--- semana15/aula50/src/index.ts
+++ semana15/aula50/src/index.ts
@@ -1,13 +1,13 @@
-import {Client} from './Client'
-import {Place}  from './Place'
+import { Client } from './Client'
+import { Place } from './Place'
 
 type person = {
   name: string
 }
 
 function createPerson(name: string): person {
-	return {name: name} 
-} 
+  return { name: name }
+}
 
 //const myPerson2 = createPerson("Robson");
 /*const myPerson3: Client = {
@@ -20,9 +20,9 @@ function createPerson(name: string): person {
   }
 }*/
 
-const novoLugar: Place = new Place("45844548")
+//const novoLugar: Place = new Place("45844548")
 
-console.log(novoLugar);
+//console.log(novoLugar);
 
 /*____________EXERCÍCIO 1____________
 a. Quais propriedades você conseguiu imprimir? Teve alguma que não foi possível? Por que isso aconteceu?
@@ -34,4 +34,13 @@ a. Mesmo sabendo que não é possível, tente criar uma instância dessa classe 
   "Cannot create an instance of an abstract class."
 b. Pense e responda: o que precisaríamos fazer para conseguir efetivamente criar uma instância dessa classe?
  Criar uma classe que receba essa classe abstrata.
+*/
+
+/*____________EXERCÍCIO 3____________
+1. O que precisaríamos fazer para conseguir efetivamente criar uma instância da classe Place? (última pergunta do exercício anterior)
+ Criar uma classe que receba essa classe abstrata.
+2. Por que a `Place` não é uma interface?
+  Porque precisa ter um acesso restrito a um dos seus atributos, o que é impossível de se fazer em interfaces.
+3. Por que a classe `Place` é uma Classe Abstrata?
+  Porque não enxergamos uma situação em que seria necessário criar uma instância dessa classe.
 */
