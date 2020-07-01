65d49975e65cbf40002dbd18ca497b50f7af714b
Melissa Tammy <mel.tammy2@gmail.com>
2020-07-01 15:58:28 -0300

    ex2

--- semana15/aula50/src/index.ts
+++ semana15/aula50/src/index.ts
@@ -28,3 +28,10 @@ console.log(novoLugar);
 a. Quais propriedades você conseguiu imprimir? Teve alguma que não foi possível? Por que isso aconteceu?
   Todas, pois não há encapsulamento.
 */
+
+/*____________EXERCÍCIO 2____________
+a. Mesmo sabendo que não é possível, tente criar uma instância dessa classe (ou seja: `new Place(...)`). Qual foi o erro que o Typescript gerou?
+  "Cannot create an instance of an abstract class."
+b. Pense e responda: o que precisaríamos fazer para conseguir efetivamente criar uma instância dessa classe?
+ Criar uma classe que receba essa classe abstrata.
+*/
