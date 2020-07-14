1a56df61555624a396a57c1e145e62fb2d434a8b
Melissa Tammy <mel.tammy2@gmail.com>
2020-07-14 17:56:49 -0300

    finalizado

--- semana17/aula56/src/index.ts
+++ semana17/aula56/src/index.ts
@@ -5,6 +5,7 @@ import { IdGenerator } from "./services/IdGenerator";
 import { UserDatabase } from "./data/UserDatabase";
 import { Authenticator } from "./services/Authenticator";
 import { HashManager } from "./services/HashManager"
+import { BaseDatabase } from "./data/BaseDataBase";
 
 dotenv.config();
 
@@ -45,6 +46,31 @@ app.post("/signup", async (req: Request, res: Response) => {
   }
 });
 
+app.get("/user/:id", async (req: Request, res: Response) => {
+  try {
+    const token = req.headers.authorization as string;
+
+    const authenticator = new Authenticator();
+    authenticator.getData(token);
+
+    const id = req.params.id;
+
+    const userDatabase = new UserDatabase();
+    const user = await userDatabase.getUserById(id);
+
+    res.status(200).send({
+      id: user.id,
+      email: user.email,
+      role: user.role,
+    });
+  } catch (err) {
+    res.status(400).send({
+      message: err.message,
+    });
+  }
+
+});
+
 app.post("/login", async (req: Request, res: Response) => {
   try {
     if (!req.body.email || req.body.email.indexOf("@") === -1) {
@@ -83,6 +109,31 @@ app.post("/login", async (req: Request, res: Response) => {
       message: err.message,
     });
   }
+});
+
+app.delete("/user/:id", async (req: Request, res: Response) => {
+  try {
+    const token = req.headers.authorization as string;
+
+    const authenticator = new Authenticator();
+    const authenticationData = authenticator.getData(token);
+
+    if (authenticationData.role !== "admin") {
+      throw new Error("Only a admin user can access this funcionality");
+    }
+
+    const id = req.params.id;
+
+    const userDatabase = new UserDatabase();
+    await userDatabase.deleteUser(id);
+
+    res.sendStatus(200);
+  } catch (err) {
+    res.status(400).send({
+      message: err.message,
+    });
+  }
+  await BaseDatabase.destroyConnection();
 });
 
 app.get("/user/profile", async (req: Request, res: Response) => {
