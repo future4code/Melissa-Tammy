eb3fc32f2b852274ee635209965dcff23ecb82e4
Melissa Tammy <mel.tammy2@gmail.com>
2020-08-06 23:07:14 -0300

    site no ar, back bugado

--- semana20/aula69/src/App.js
+++ semana20/aula69/src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import {FileUploader} from './components/FileUploader'
 function App() {
   return (
     <div className="App">
--- semana20/aula69/src/components/FileUploader.js
+++ semana20/aula69/src/components/FileUploader.js
@@ -1,26 +1,30 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 
 export const FileUploader = () => {
 
     const [link, setLink] = useState(undefined)
 
-    const handleFile = async (event) =>{
-        try{
+    const handleFile = async (event) => {
+        try {
             const data = new FormData();
             data.append('file', event.target.files[0])
 
-            const res = await axios.put('https://g5vr718si2.execute-api.us-east-1.amazonaws.com/dev', data);
+            const res = await axios.put('https://g5vr718si2.execute-api.us-east-1.amazonaws.com/dev/files/upload', data);
             setLink(res.data.link)
-        }catch(e){
+        } catch (e) {
             console.log(e.message)
         }
     }
-    return(
-        <>
-            <input type={'file'} onChange={handleFile}/>
-            <img src={link}></img>
-            <p><a href={link}>Link do arquivo</a></p>
-        </>
+    return (
+        <div>
+            <input type={'file'} onChange={handleFile} />
+            {link && (
+                <div>
+                    <img src={link} alt={'Minha imagem'} />
+                    <p><a href={link}>Link para o arquivo</a></p>
+                </div>
+            )}
+        </div>
     )
 }
